0e18b8641d625a8cac5511b4f51175da18fd49b3
DrokoDomi <droko@gmx.de>
2021-11-21 15:23:14 +0100

    Refactor of functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,9 @@
+import time
 import unittest
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,6 +12,13 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self) -> None:
         self.browser.quit()
+
+    def check_for_row_list_in_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(
+            row_text, [row.text for row in rows],
+        )
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -29,8 +36,7 @@ class NewVisitorTest(unittest.TestCase):
             inputbox.get_attribute("placeholder"),
             "Enter a to-do item"
         )
-        
-        
+
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
         inputbox.send_keys("Buy peacock feathers")
@@ -39,6 +45,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_list_in_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -46,16 +53,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        
+
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn(
-            "1: Buy peacock feathers", [row.text for row in rows],
-        )
-        self.assertIn(
-            "2: Use peacock feathers to make a fly", [row.text for row in rows],
-        )
+        self.check_for_row_list_in_table("1: Buy peacock feathers")
+        self.check_for_row_list_in_table("2: Use peacock feathers to make a fly")
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
