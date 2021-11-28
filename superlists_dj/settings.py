760df17bb48c50f387464631846f9faeeb982e32
DrokoDomi <droko@gmx.de>
2021-11-28 12:53:13 +0100

    hack ALLOWED_HOSTS to be *

--- superlists_dj/settings.py
+++ superlists_dj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qs^ex^w)wctf!m^=ep676oa!)vw_)t(id_dk15q1&g6wwmh5xf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
