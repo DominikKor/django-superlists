8f689deb6c5c8bd6ddae879821039cbaf2a26fcc
DrokoDomi <droko@gmx.de>
2021-11-28 15:59:34 +0100

    Fix settings, ALLOWED_HOSTS got overwritten

--- superlists_dj/settings.py
+++ superlists_dj/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qs^ex^w)wctf!m^=ep676oa!)vw_)t(id_dk15q1&g6wwmh5xf'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 if "DJANGO_DEBUG_FALSE" in os.environ:
     DEBUG = False
@@ -31,8 +29,6 @@ else:
     DEBUG = True
     SECRET_KEY = "insecure-key-for-dev"
     ALLOWED_HOSTS = []
-
-ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
