fc6d93478ab904fa50050db46277f95b32ade53a
DrokoDomi <droko@gmx.de>
2021-12-02 22:39:06 +0100

    Refactor functional_tests/tests.py - Remove phantom sleeps

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,21 +1,24 @@
 import os
 import time
-import unittest
 
 import django
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.contrib.staticfiles.testing import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver import FirefoxOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver import FirefoxOptions
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists_dj.settings")
 django.setup()
 
-class NewVisitorTest(StaticLiveServerTestCase):
+MAX_WAIT = 10
+
+
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self) -> None:
-        opts =  FirefoxOptions()
-        opts.add_argument("--headless")
+        opts = FirefoxOptions()
+        # opts.add_argument("--headless")
         self.browser = webdriver.Firefox(options=opts)
         staging_server = os.environ.get("STAGING_SERVER")
         print("Staging server:", staging_server)
@@ -25,12 +28,18 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
-    def check_for_row_list_in_table(self, row_text):
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn(
-            row_text, [row.text for row in rows],
-        )
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, "id_list_table")
+                rows = table.find_elements(By.TAG_NAME, "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.1)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -56,19 +65,17 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_list_in_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        self.check_for_row_list_in_table("1: Buy peacock feathers")
-        self.check_for_row_list_in_table("2: Use peacock feathers to make a fly")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
@@ -77,4 +84,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-
