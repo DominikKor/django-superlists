62410c608ad92285ecc4ea2ea48414ad4f41589f
DrokoDomi <droko@gmx.de>
2021-12-05 20:59:53 +0100

    Moved url.py to lists/ and included in base urls

--- superlists_dj/urls.py
+++ superlists_dj/urls.py
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import re_path
-
-from lists import views
+from django.urls import re_path, include
+from lists import urls as list_urls
+from lists import views as list_views
 
 urlpatterns = [
-    re_path(r'^$', views.home_page, name='home'),
-    re_path(r'^lists/new$', views.new_list, name='new_list'),
-    re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+    re_path(r'^$', list_views.home_page, name='home'),
+    re_path(r'^lists/', include(list_urls)),
 ]
