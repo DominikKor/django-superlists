3df9e09ddc5bbb2c3405f735120512f8643f32dd
DrokoDomi <droko@gmx.de>
2021-12-12 22:34:13 +0100

    Notes and template config file for provisioning

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,15 @@
+import random
+from fabric.contrib.files import append, exists
+from fabric.api import cd, env, local, run
+
+REPO_URL = 'https://github.com/DrokoDomi/superlists.git'
+
+def deploy():
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    run(f'mkdir -p {site_folder}')
+    with cd(site_folder):
+        _get_latest_source()
+        _update_virtualenv()
+        _create_or_update_dotenv()
+        _update_static_files()
+        _update_database()
