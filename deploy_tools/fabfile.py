7feabacabcbda99e17dfdded84b6ac1df53144a6
DrokoDomi <droko@gmx.de>
2021-12-13 07:00:44 +0100

    Fix trailing / at end of directory in fabric3 deploy_tools/fabfile.py

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -33,7 +33,7 @@ def _create_or_update_dotenv():
 
 
 def _update_static_files():
-    run('./virtualenv/bin/python/ manage.py collectstatic --noinput')
+    run('./virtualenv/bin/python manage.py collectstatic --noinput')
 
 
 def _update_database():
