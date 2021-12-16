55249eea939a63e6631cf0a2c7d49019b01e6207
DrokoDomi <droko@gmx.de>
2021-12-16 22:09:55 +0100

    Created FT to prevent adding empty items. Not passing.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,4 +1,6 @@
 from selenium.webdriver import FirefoxOptions
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 from functional_tests.base import FunctionalTest
 
@@ -11,16 +13,34 @@ OPTS = FirefoxOptions()
 
 class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
-        # Edith goes to the home page and accidentally tries to submit an empty list item.
-        # She hits enter on the empty input box
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
-        # The home page refreshes, and there is an error message saying that list items cannot be blank.
+        # The home page refreshes, and there is an error message saying
+        # that list items cannot be blank
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
+            "You can't have an empty list item"
+        ))
 
         # She tries again with some text for the item, which now works
+        self.browser.find_element(By.ID, 'id_new_item').send_keys('Buy milk')
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
 
         # Perversely, she now decides to submit a second blank list item
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
 
         # She receives a similar warning on the list page
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
+            "You can't have an empty list item"
+        ))
 
-        # And she can correct ir by filling some text in.
-        self.fail("write me!")
+        # And she can correct it by filling some text in
+        self.browser.find_element(By.ID, 'id_new_item').send_keys('Make tea')
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
